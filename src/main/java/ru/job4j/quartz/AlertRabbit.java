ec4f8fda82be1cfe8584353daadc4d02705fb886
Alexlife9 <crashmx@inbox.ru>
2022-08-25 00:10:59 +0300

    Quartz

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -2,6 +2,11 @@ package ru.job4j.quartz;
 
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import static org.quartz.JobBuilder.*;
 import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
@@ -10,11 +15,19 @@ import static org.quartz.SimpleScheduleBuilder.*;
  * Quartz
  *
  * @author Alex_life
- * @version 1.0
- * @since 23.08.2022
+ * @version 2.0
+ * добавил файл rabbit.properties для чтения настроек
+ * @since 24.08.2022
  */
 public class AlertRabbit {
     public static void main(String[] args) {
+        Properties properties = new Properties();
+        try (InputStream in = AlertRabbit.class.getClassLoader()
+                .getResourceAsStream("rabbit.properties")) {
+            properties.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         try {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler(); /* 1.Конфигурирование */
             /* Начало работы происходит с создания класса управляющего всеми работами.
@@ -23,7 +36,8 @@ public class AlertRabbit {
             JobDetail job = newJob(Rabbit.class).build(); /* 2.Создание задачи.
                                                           quartz каждый раз создает объект с типом org.quartz.Job */
             SimpleScheduleBuilder times = simpleSchedule() /* 3.Создание расписания */
-                    .withIntervalInSeconds(10) /* Конструкция настраивает периодичность запуска */
+                    .withIntervalInSeconds(Integer.parseInt(
+                            properties.getProperty("rabbit.interval"))) /* читаем строчку из пропертис */
                     .repeatForever();          /* Запускаем задачу через 10 секунд и делаем это бесконечно */
             Trigger trigger = newTrigger() /* 4.Задача выполняется через триггер */
                     .startNow()            /* Здесь можно указать, когда начинать запуск. Например сразу */
@@ -41,10 +55,8 @@ public class AlertRabbit {
      */
     public static class Rabbit implements Job {
         @Override
-        public void execute(JobExecutionContext context) throws JobExecutionException {
+        public void execute(JobExecutionContext context) {
             System.out.println("Rabbit runs here ...");
         }
     }
-
-
 }
