bf1950213091f6e38c52a83bbf9dfe7ea3f7f3bb
Alexlife9 <crashmx@inbox.ru>
2022-08-25 01:15:28 +0300

    Job c параметрами

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -5,6 +5,10 @@ import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
@@ -12,14 +16,36 @@ import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
 /**
- * Quartz
+ * Job c параметрами
  *
  * @author Alex_life
- * @version 2.0
- * добавил файл rabbit.properties для чтения настроек
- * @since 24.08.2022
+ * @version 3.0
+ * добавил коннект к базе при старте. Объект коннект будет передаваться в Job.
+ * @since 25.08.2022
  */
 public class AlertRabbit {
+
+    private static Properties properties;
+
+    public AlertRabbit(Properties properties) {
+        AlertRabbit.properties = properties;
+    }
+
+    public static Connection initConnection() throws ClassNotFoundException {
+        Class.forName(properties.getProperty("driver"));
+        try {
+            Connection connection = DriverManager.getConnection(
+                    properties.getProperty("url"),
+                    properties.getProperty("login"),
+                    properties.getProperty("password")
+            );
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return initConnection();
+    }
+
+
     public static void main(String[] args) {
         Properties properties = new Properties();
         try (InputStream in = AlertRabbit.class.getClassLoader()
@@ -28,13 +54,19 @@ public class AlertRabbit {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        try {
+        try (Connection cn = initConnection()) {
+            List<Long> store = new ArrayList<>();
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler(); /* 1.Конфигурирование */
             /* Начало работы происходит с создания класса управляющего всеми работами.
                В объект Scheduler мы будем добавлять задачи, которые хотим выполнять периодически */
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build(); /* 2.Создание задачи.
-                                                          quartz каждый раз создает объект с типом org.quartz.Job */
+            JobDataMap data = new JobDataMap(); /* При создании Job указываем параметры data,
+                                                   в которые передаем ссылку на store */
+            data.put("store", store);           /* В данном примере store это ArrayList.*/
+            data.put("connection", cn);
+            JobDetail job = newJob(Rabbit.class) /* 2.Создание задачи */
+                    .usingJobData(data)
+                    .build();
             SimpleScheduleBuilder times = simpleSchedule() /* 3.Создание расписания */
                     .withIntervalInSeconds(Integer.parseInt(
                             properties.getProperty("rabbit.interval"))) /* читаем строчку из пропертис */
@@ -44,7 +76,10 @@ public class AlertRabbit {
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger); /* 5.Загрузка задачи и триггера в планировщик */
-        } catch (SchedulerException se) {
+            Thread.sleep(10000); /* приостанавливаем работу на указанное время (1/1000 часть секунды) */
+            scheduler.shutdown(); /* завершаем работу */
+            System.out.println(store); /* выводим в консоль записи из аррэйлиста */
+        } catch (SchedulerException | InterruptedException | ClassNotFoundException | SQLException se) {
             se.printStackTrace();
         }
     }
@@ -54,9 +89,27 @@ public class AlertRabbit {
      * в данном случае - вывод в консоль
      */
     public static class Rabbit implements Job {
+
+        public Rabbit() {
+            System.out.println(hashCode());
+        }
+
+        /**
+         * Каждый запуск работы вызывает конструктор.
+         * Чтобы в объекте Job иметь общий ресурс нужно использовать JobExecutionContext.
+         */
         @Override
         public void execute(JobExecutionContext context) {
             System.out.println("Rabbit runs here ...");
+            Connection connection = (Connection) context.getJobDetail().getJobDataMap().get("connection");
+            try (PreparedStatement statement = connection.prepareStatement(
+                    "insert into rabbits (created_date) values (?)"
+            )) {
+                statement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now()));
+                statement.execute();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
