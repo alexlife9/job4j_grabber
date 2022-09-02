3deb9b2b33fd03aa50e01e3c49427282f464cd27
Alexlife9 <crashmx@inbox.ru>
2022-09-02 21:02:23 +0300

    Модель данных - Post

---
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -0,0 +1,62 @@
+package ru.job4j.grabber;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+/**
+ * Модель данных - Post
+ *
+ * @author Alex_life
+ * @version 1.0
+ * @since 02.09.2022
+ */
+public class Post {
+    /**
+     *  - id типа int - идентификатор вакансии (берется из нашей базы данных);
+     *  - title типа String - название вакансии;
+     *  - link типа String - ссылка на описание вакансии;
+     *  - description типа String - описание вакансии;
+     *  - created типа LocalDateTime - дата создания вакансии.
+     */
+    private final int id;
+    private final String title;
+    private final String link;
+    private final String description;
+    private final LocalDateTime created;
+
+    public Post(int id, String title, String link, String description, LocalDateTime created) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return id == post.id && Objects.equals(link, post.link);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, link);
+    }
+
+    @Override
+    public String toString() {
+        return "Post{"
+                + "id=" + id
+                + ", title='" + title + '\''
+                + ", link='" + link + '\''
+                + ", description='" + description + '\''
+                + ", created=" + created
+                + '}';
+    }
+}
