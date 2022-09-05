ad02ef08e88dea8223d23d451ed0caf20b9515fc
Alexlife9 <crashmx@inbox.ru>
2022-09-06 01:05:33 +0300

    Схема таблицы Post

--- pgsql/aggregator.sql
+++ pgsql/aggregator.sql
@@ -3,8 +3,8 @@ create schema aggregator;
 create table post
 (
     id      serial primary key,
-    name    text,        -- название вакансии
-    text    text,        -- описание вакансии
-    link    text UNIQUE, -- ссылка на вакансию
-    created date         -- дата создания
+    name    text,                -- название вакансии
+    text    text,                -- описание вакансии
+    link    varchar(250) UNIQUE, -- ссылка на вакансию
+    created date                 -- дата создания
 );
